ac4ff13b04a40cfbe4ce8c114d76bee2ef3a454e
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-05 16:38:30 -0500

    CardGame class with constructor properties only. No gameplay added yet

---
+++ deck.rb
@@ -0,0 +1,36 @@
+FILE = ARGV[0]
+
+class CardGame
+  attr_reader :cards
+  def initialize
+    @data_for_cards = []
+    @cards = assemble_cards
+  end
+
+  def assemble_cards
+    front = 0
+    back = 1
+    data_for_cards.each_slice(2) |card|
+      @card << Card.new(slice[front], slice[back])
+    end
+  end  
+
+  def parse_card_file
+    cards_file = File.open(FILE, 'r')
+    cards_file.each_with_index do |line,index|
+      unless index % 3 == 2
+        @data_for_cards << line.chomp
+      end  
+    end
+  end
+end
+
+class GameDisplay #this is just for terminal input and output
+  attr_reader :filename
+  def initialize(file)
+    @card_game = CardGame.new
+  end
+end
+
+cg = CardGame.new
+cg.parse_card_file
