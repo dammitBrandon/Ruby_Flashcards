6b294f0e96b05503cb4931b73c99d686865ed4c8
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-05 16:38:02 -0500

    cards model created

--- cards.rb
+++ cards.rb
@@ -1,1 +1,19 @@
 #this will have the card class
+class Card
+  attr_reader :definition, :answer
+  def initialize(args)
+    @definition = args[:definition]
+    @answer = args[:answer]
+  end
+
+  def check_answer(answer)
+    answer = answer.downcase
+    return (answer== @answer.downcase)
+  end
+
+end
+
+args = {definition: "Designates code that must be run unconditionally at the beginning of the program before any other.", answer: "BEGIN"}
+card = Card.new(args)
+p card
+p card.check_answer("begin")
